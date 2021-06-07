0878c17abc9a9f86a5a250c6dba8d36486735dd4
AyBalatsan <AyBalatsan@yandex.ru>
2021-06-07 16:51:38 +0600

    v5

--- source/js/slayder.js
+++ source/js/slayder.js
@@ -23,5 +23,25 @@ dots.forEach((item) =>
 );
 
 showSlides(slideIndex);
-window.addEventListener('resize', showSlides(slideIndex));
-window.onresize = showSlides(slideIndex);
+window.addEventListener('resize', () => {
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    let i;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("toggle-round__item--active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " toggle-round__item--active";
+  }
+  else{
+    let i;
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+      for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("toggle-round__item--active", "");
+    }
+  }
+});
