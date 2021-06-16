f2904b498917b1e384ff6f43ed27650328d5bcc3
AyBalatsan <AyBalatsan@yandex.ru>
2021-06-17 02:13:13 +0600

    v9

--- source/js/authorization.js
+++ source/js/authorization.js
@@ -1,18 +1,14 @@
 const lock = document.querySelector('.page-header__avtor');
 const authorization = document.querySelector('.authorization');
 const close = document.querySelector('.authorization__close');
+const blure = document.querySelector('.after-modal');
 
 lock.addEventListener('click', (evt)=>{
   evt.preventDefault();
   authorization.classList.toggle('authorization--active');
+  blure.classList.toggle('blure--active');
 });
 close.addEventListener('click', (evt)=>{
-  evt.preventDefault();
   authorization.classList.toggle('authorization--active');
+  blure.classList.toggle('blure--active');
 });
-// const popUp = (target) =>{
-//   target.addEventListener('click', (evt)=>{
-//     evt.preventDefault();
-//     authorization.classList.toggle('authorization--active');
-//   });
-// }
